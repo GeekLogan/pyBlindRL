ed3e24548964a68d092ae1bda60ef9cc5a18702d
Logan Walker <loganaw@umich.edu>
2024-04-15 15:30:28 -0400

    Update test_methods.py

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -6,7 +6,7 @@
 from __future__ import annotations
 
 # from pyBlindRL.hello_world import hello_world
-import pyBlindRL
+from pyBlindRL import *
 
 def test_gaussian():
     pyBlindRL.gaussian_3d( (100,100,100) )
