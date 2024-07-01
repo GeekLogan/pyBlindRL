9e0b7cc80699e33182333ae42c7644f51ce8f3fc
Jacob Eggerd <jfeggerd@gmail.com>
2024-07-01 12:47:03 -0400

    Added cuda device

--- src/pyBlindRL/testing.py
+++ src/pyBlindRL/testing.py
@@ -18,6 +18,8 @@ output_functions_dir = output_dir + "/functions"
 imgs_dir = output_dir + "/imgs"
 initial_functions_dir = output_dir + "/initial_functions"
 
+device = torch.device("cuda", 0)
+
 vol = CloudVolume('precomputed://https://ntracer2.cai-lab.org/data2/051524_bitbow_ch0', parallel=True, progress=True)
 
 x_lim = 28000
@@ -58,7 +60,7 @@ for img in tqdm(imgs):
     img_tensor = torch.from_numpy(np.array(img).astype(np.int16))
 
     psf_guess = generate_initial_psf(img_tensor)
-    output_img, output_psf = RL_deconv_blind(img_tensor, torch.from_numpy(psf_guess), device='cuda')
+    output_img, output_psf = RL_deconv_blind(img_tensor, torch.from_numpy(psf_guess), device=device)
 
     tiff.imwrite(imgs_dir + "/img_" + str(num) + ".tiff", img_tensor.detach().cpu().numpy())
     # tiff.imwrite(blurred_dir + "/img_" + str(num) + ".png", blurred)
