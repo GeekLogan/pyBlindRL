ba469f4dccc50b32e24fd412f132e5dca3beffcb
Jacob Eggerd <jfeggerd@gmail.com>
2024-07-02 10:50:37 -0400

    More benchmarking

--- src/pyBlindRL/testing.py
+++ src/pyBlindRL/testing.py
@@ -2,6 +2,7 @@ from commands import generate_initial_psf, RL_deconv_blind
 from utility import clear_dir
 import scipy
 import cv2
+import time
 import tifffile as tiff
 import torch
 import os
@@ -21,57 +22,79 @@ initial_functions_dir = output_dir + "/initial_functions"
 
 device = torch.device("cuda", 0)
 
-vol = CloudVolume('precomputed://https://ntracer2.cai-lab.org/data2/051524_bitbow_ch0', parallel=True, progress=True)
 
 x_lim = 28000
 y_lim = 28000
 count = 0
 
-imgs = []
+def deconvolve_cloud(x, y, z, xy_size, slices, section_size, blur_radius, device, output_dir):
+    vol = CloudVolume('precomputed://https://ntracer2.cai-lab.org/data2/051524_bitbow_ch0', parallel=True, progress=True)
 
-dirs = [blurred_dir, deconv_dir, output_functions_dir, imgs_dir, initial_functions_dir]
-
-for i in dirs:
-    clear_dir(i)
-
-count = 0
-
-while count < 1:
-    z = random.randint(300,1000)
-    x = random.randint(1, (x_lim / 2000)) * 2000
-    y = random.randint(1, (y_lim / 2000)) * 2000
-
-    img = vol[x-1000:x, y-1000:y, z-64:z]
+    img = vol[x:x+xy_size, y:y+xy_size, z:z+slices]
 
     img = img [:, :, :, 0]
 
     img = img.transpose(2, 0, 1)
 
-    if np.std(img) < 17:
-        continue
+    blurred_dir = output_dir + "/blurred"
+    deconv_dir = output_dir + "/deconv"
+    output_functions_dir = output_dir + "/functions"
+    imgs_dir = output_dir + "/imgs"
+    initial_functions_dir = output_dir + "/initial_functions"
 
-    count += 1
+    dirs = [blurred_dir, deconv_dir, output_functions_dir, imgs_dir, initial_functions_dir]
 
-    imgs.append(img)
-
-num = 0
-
-for img in tqdm(imgs):
+    for i in dirs:
+        clear_dir(i)
 
     img_tensor = torch.from_numpy(np.array(img).astype(np.int16))
 
-    blurred = scipy.ndimage.gaussian_filter(img, (1, 1, 2), radius = 10)
+    blurred = scipy.ndimage.gaussian_filter(img, (1, 1, 2), radius = blur_radius)
 
-    blurred = torch.from_numpy(blurred.astype(np.int16))
 
-    psf_guess = generate_initial_psf(blurred)
+    output_img = np.zeros((img_tensor.shape))
 
-    output_img, output_psf = RL_deconv_blind(blurred, torch.from_numpy(psf_guess), target_device=device, iterations=20)
+    num = 0
 
-    tiff.imwrite(imgs_dir + "/img_" + str(num) + ".tiff", img_tensor.detach().cpu().numpy())
-    tiff.imwrite(blurred_dir + "/img_" + str(num) + ".tiff", blurred.detach().cpu().numpy())
-    tiff.imwrite(deconv_dir + "/img_" + str(num) + ".tiff", output_img)
-    tiff.imwrite(output_functions_dir + "/img_" + str(num) + ".tiff", output_psf)
-    tiff.imwrite(initial_functions_dir + "/img_" + str(num) + ".tiff", psf_guess)
+    for i in tqdm(range(int(xy_size / section_size))):
+        for j in range(int(xy_size / section_size)):
+            blurred_section = torch.clone(torch.from_numpy((blurred[:, (i*section_size):((i+1) * section_size), (j*section_size):((j+1) * section_size)]).astype(np.int16)))
 
-    num += 1
+            psf_guess = generate_initial_psf(blurred_section)
+
+            output, output_psf = RL_deconv_blind(blurred_section, torch.from_numpy(psf_guess), target_device=device, iterations=20)
+
+            output_img[:, (i*section_size):((i+1) * section_size), (j*section_size):((j+1) * section_size)] = output
+
+            tiff.imwrite(imgs_dir + "/img_" + str(num) + ".tiff", img_tensor.detach().cpu().numpy())
+            tiff.imwrite(blurred_dir + "/img_" + str(num) + ".tiff", blurred)
+            tiff.imwrite(deconv_dir + "/img_" + str(num) + ".tiff", output_img.astype(np.uint16))
+            tiff.imwrite(output_functions_dir + "/img_" + str(num) + ".tiff", output_psf)
+            tiff.imwrite(initial_functions_dir + "/img_" + str(num) + ".tiff", psf_guess)
+
+            num += 1
+
+seconds = time.time()
+
+deconvolve_cloud(7000, 10000, 493, 1000, 64, 1000, 5, device, "./outputs")
+
+print("Total Time (1 section): " + str(time.time() - seconds) + " seconds")
+
+
+seconds = time.time()
+
+deconvolve_cloud(7000, 10000, 493, 1000, 64, 500, 5, device, "./outputs")
+
+print("Total Time (4 sections): " + str(time.time() - seconds) + " seconds")
+
+seconds = time.time()
+
+deconvolve_cloud(7000, 10000, 493, 1000, 64, 100, 5, device, "./outputs")
+
+print("Total Time (100 section): " + str(time.time() - seconds) + " seconds")
+
+seconds = time.time()
+
+deconvolve_cloud(7000, 10000, 493, 1000, 64, 50, 5, device, "./outputs")
+
+print("Total Time (400 sections): " + str(time.time() - seconds) + " seconds")
